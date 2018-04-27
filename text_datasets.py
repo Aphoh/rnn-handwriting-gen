ef399ca64129e72e0e6955956e8408abd14772e1
William Arnold <will748@gmail.com>
2018-04-27 11:52:42 -0700

    Modularize text datasets

--- text_datasets.py
+++ text_datasets.py
@@ -15,44 +15,34 @@ import tensorflow as tf
 #
 # python text_datasets.py will run this example.
 
-
-class Gutenberg():
-    def __init__(self, sentence_size=100, batch_size=100):
+class NltkDataset():
+    def __init__(self, nltk_module, sentence_size, batch_size):
         print("Making Gutenberg dataset...")
         self.sentence_size=sentence_size
         self.batch_size=batch_size
-        self.fileids = gutenberg.fileids()
+        self.fileids = nltk_module.fileids()
         chars = []
         for f in self.fileids:
-            chars.extend(map(lambda x: ord(x), gutenberg.raw(f)))
+            chars.extend(map(lambda x: ord(x), nltk_module.raw(f)))
         chars = np.array(chars)
         # Split into batches
         n = chars.size // sentence_size
         chars = chars[:n * sentence_size]
-        self.batches = np.array(np.split(chars, n)) # Splits into lists of size sentence_size
+        self.sentences = np.array(np.split(chars, n)) # Splits into lists of size sentence_size
+        self.batches = self.sentences.T
         self.dset = tf.data.Dataset.from_tensor_slices(self.batches)
 
     def tf_iterator(self):
         return self.dset.batch(self.batch_size).make_one_shot_iterator()
+
+class Gutenberg(NltkDataset):
+    def __init__(self, sentence_size=100, batch_size=100):
+        NltkDataset.__init__(self, gutenberg, sentence_size, batch_size)
 
 class Reuters():
     def __init__(self, sentence_size=100, batch_size=100):
-        print("Making Reuters dataset...")
-        self.sentence_size=sentence_size
-        self.batch_size=batch_size
-        self.fileids = reuters.fileids()
-        chars = []
-        for f in self.fileids:
-            chars.extend(map(lambda x: ord(x), reuters.raw(f)))
-        chars = np.array(chars)
-        # Split into batches
-        n = chars.size // sentence_size
-        chars = chars[:n * sentence_size]
-        self.batches = np.array(np.split(chars, n)) # Splits into lists of size sentence_size
-        self.dset = tf.data.Dataset.from_tensor_slices(self.batches)
+        NltkDataset.__init__(self, reuters, sentence_size, batch_size)
 
-    def tf_iterator(self):
-        return self.dset.batch(self.batch_size).make_one_shot_iterator()
 
 if __name__ == "__main__":
     sess = tf.Session()
